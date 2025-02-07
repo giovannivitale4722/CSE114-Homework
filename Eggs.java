d672b9b6bfdddbb6e05633e5106b641b1e74e92a
giovannivitale4722 <136027772+giovannivitale4722@users.noreply.github.com>
2025-02-07 00:21:32 -0500

    Update Eggs.java
    finished 2/7/25

--- Eggs.java
+++ Eggs.java
@@ -3,6 +3,15 @@ public class Eggs {
     public static void main(String[] args){
         System.out.print("Number of eggs in the order: ");
         Scanner input = new Scanner(System.in);
-        double cost 
+        int eggs = input.nextInt();
+        int dozen = eggs / 12;
+        int loose_eggs = eggs % 12;
+        int eggsByDozen = dozen * 12;
+        double dozenCost = dozen * 3.25;
+        double loose_eggs_cost = loose_eggs * 0.45;
+        double totalCost = dozenCost + loose_eggs_cost;
+        System.out.println("You ordered " + eggs + " eggs. That's " + dozen + " dozen (" + dozen + " x 12 = " + eggsByDozen + " eggs) at $3.25 per dozen (" + dozen + " x $3.25 = $" + dozenCost + ") and " + loose_eggs + " loose eggs at 45 cents (" + loose_eggs + " x 45c = $" + loose_eggs_cost + ") for a total of $" + totalCost + " ($" + dozenCost + " + $" + loose_eggs_cost + " = $" + totalCost + ").");
+        input.close();
+
     }
 }

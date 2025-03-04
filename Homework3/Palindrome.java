4299322747256343d7d48443944026a116ca2a9a
giovannivitale4722 <136027772+giovannivitale4722@users.noreply.github.com>
2025-03-04 18:40:43 -0500

    Create Palindrome.java

---
+++ Homework3/Palindrome.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Palindrome{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the input string: ");
+        String str = input.nextLine();
+        System.out.println("Input string " + str + palindrome(str));
+        input.close();
+        
+    }
+    public static String palindrome(String str){
+        char[] stringArray = str.toCharArray();
+        for (int i = 0, j = stringArray.length -1; i < stringArray.length/2; i++, j--){
+            if (stringArray[i] != stringArray[j]){
+                return " is not a palindrome";
+            }
+            
+            
+        }
+        return " is a palindrome";
+    }
+}

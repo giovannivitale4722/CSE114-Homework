8e6ae89810cb3b6082db1b4bc6ff59565099195f
giovannivitale4722 <136027772+giovannivitale4722@users.noreply.github.com>
2025-03-08 15:02:59 -0500

    Update Emirp.java
    optimzed code + had an issue where i didn't check if n was equal to its reverse

--- Homework3/Emirp.java
+++ Homework3/Emirp.java
@@ -1,35 +1,33 @@
 public class Emirp {
     public static void main(String[] args) {
         int count = 0;
-        for (int n = 13; count < 100; n++){
-            if (prime(n) && non_palindrome(n)){
+        for (int n = 13; count < 100; n++) {
+            if (prime(n) && non_palindrome(n)) {
                 System.out.print(n + " ");
                 count++;
             }
         }
-        }
-    public static boolean prime(int n){
-      for (int i = 2; i < n/2; i++){
-        if (n % i == 0){
-            return false;
-        }
-      }
-      return true;
-
     }
-    public static boolean non_palindrome(int n){
-        String strN= Integer.toString(n);
-        String reversed_n_string = "";
-        for (int i = strN.length() - 1; i >= 0; i--){
-            reversed_n_string += strN.charAt(i);
-        }
-        int reversed_n_int = Integer.parseInt(reversed_n_string);
-
-        for (int i = 2; i <reversed_n_int/2; i++){
-            if (reversed_n_int % i == 0){
+    public static boolean prime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i <= Math.sqrt(n); i++) {  
+            if (n % i == 0) {
                 return false;
             }
         }
         return true;
     }
+    public static boolean non_palindrome(int n) {
+        int reversed_n_int = reverseNumber(n);
+        return (n != reversed_n_int) && prime(reversed_n_int); 
+    }
+    public static int reverseNumber(int n) {
+        int reversed = 0;
+        while (n > 0) {
+            reversed = reversed * 10 + (n % 10);
+            n /= 10;
+        }
+        return reversed;
+    }
 }
+
